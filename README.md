35915865244d87cd1b265a1e8596b1515f040d92
justincbal <justinseebee@hotmail.com>
2023-01-19 21:27:55 -0500

    v2 complete

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# registration-form
+# Registration form
+
+## Description
+This is a basic sign up page using HTML, CSS, and Javascript. I used Javascript to do form validation.
+
+## Features
+- Form validation
+    -  Empty fields
+    -  Check valid email
+    -  Check password match
---
+++ script.js
@@ -0,0 +1,77 @@
+const fName = document.querySelector('.fName');
+const lName = document.querySelector('.lName');
+const email = document.querySelector('.email');
+const pass1 = document.querySelector('.pass');
+const pass2 = document.querySelector('.passCon');
+
+
+document.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    validateForm();
+
+})
+
+const validateEmail = (email) => {
+    var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; 
+    return pattern.test(String(email).toLocaleLowerCase());
+}
+
+const validateForm = () => {
+
+    if (fName.value === '') {
+        setErr(fName, 'First name required!');
+    }
+    else {
+        setSucc(fName);
+    }
+
+    if (lName.value === '') {
+        setErr(lName, 'Last name required!');
+    }
+    else {
+        setSucc(lName);
+    }
+
+    if (email.value === '') {
+        setErr(email, 'Email is required');
+    }
+    else if (!validateEmail(email.value)) {
+        setErr(email, 'Invalid email address');
+    }
+    else {
+        setSucc(email);
+    }
+
+    if (pass1.value === '') {
+        setErr(pass1, 'Enter a password!');
+    }
+    else {
+        setSucc(pass1);
+    }
+
+    if (pass2.value !== pass1.value) {
+        console.log(pass1.value, pass2.value);
+        setErr(pass2, 'Passwords do not match!');
+    }
+    else {
+        setSucc(pass2);
+    }
+
+    
+
+
+}
+
+const setErr = (elm, message) => {
+    const element = elm.parentElement;
+    const errorMessage = element.querySelector('.error');
+
+    errorMessage.textContent = message;
+}
+
+const setSucc = elm => {
+    const element = elm.parentElement;
+    const remMessage = element.querySelector('.error');
+    remMessage.textContent = '';
+}
